1ce5a681682eddc565c267521b8ecd2639ec5273
Donghee Lee <stringdong@gmail.com>
2020-01-17 15:31:13 +0900

    Copy project

---
+++ IOS_SimpleTodo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  IOS_SimpleTodo
+//
+//  Created by 이동희A(Lee, DongHee) on 2020/01/17.
+//  Copyright © 2020 Donghee Lee. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ IOS_SimpleTodo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  IOS_SimpleTodo
+//
+//  Created by 이동희A(Lee, DongHee) on 2020/01/17.
+//  Copyright © 2020 Donghee Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
