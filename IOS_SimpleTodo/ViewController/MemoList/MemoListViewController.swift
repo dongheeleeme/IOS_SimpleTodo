b6ad35cc33baad6e344f96c2ff79d3d4feb85d22
Donghee Lee <stringdong@gmail.com>
2020-01-17 15:48:05 +0900

    Modify directories structure

--- IOS_SimpleTodo/ViewController/MemoList/MemoListViewController.swift
+++ IOS_SimpleTodo/ViewController/MemoList/MemoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MemoListViewController.swift
 //  IOS_SimpleTodo
 //
 //  Created by 이동희A(Lee, DongHee) on 2020/01/17.
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MemoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 }
-
